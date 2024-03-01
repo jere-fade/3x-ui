d1752b595824f95e4296250aa11617a44111f020
jeremy <jeremyhuacs@gmail.com>
2024-03-01 19:52:26 +0800

    add subscription userinfo header

--- sub/subController.go
+++ sub/subController.go
@@ -2,7 +2,10 @@ package sub
 
 import (
 	"encoding/base64"
+	"fmt"
 	"strings"
+
+	"x-ui/web/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -51,25 +54,55 @@ func (a *SUBController) initRouter(g *gin.RouterGroup) {
 
 func (a *SUBController) subs(c *gin.Context) {
 	subId := c.Param("subid")
-	host := strings.Split(c.Request.Host, ":")[0]
-	subs, header, err := a.subService.GetSubs(subId, host)
-	if err != nil || len(subs) == 0 {
-		c.String(400, "Error!")
-	} else {
-		result := ""
-		for _, sub := range subs {
-			result += sub + "\n"
+	var myproxySub = "1ba393cfe9ecf4c2b545d60aa21c60840a41aacc899231bb21d2d0613bdb9a97"
+	var myproxyTitle = "myproxy++"
+	var lanSub = "1f7e4c7913c53240acc1bdc8a3e48f260120faf5c504ea9f2aa7b51d8916497d"
+	var lanTitle = "LAN"
+	if subId == myproxySub || subId == lanSub {
+		inboundService := service.InboundService{}
+		inbounds, err := inboundService.GetAllInbounds()
+
+		if err != nil {
+			c.String(400, "Error!")
+		}
+		var upload, download, total int64
+		for _, inbound := range inbounds {
+			upload += inbound.Up
+			download += inbound.Down
+			total += inbound.Total
+		}
+		header := fmt.Sprintf("upload=%d; download=%d; total=%d; expire=%d", upload, download, total, 0)
+		c.Writer.Header().Set("Subscription-Userinfo", header)
+
+		if subId == myproxySub {
+			c.Writer.Header().Set("Profile-Title", myproxyTitle)
+			c.File("clash" + "/" + myproxyTitle)
+		} else {
+			c.Writer.Header().Set("Profile-Title", lanTitle)
+			c.File("clash" + "/" + lanTitle)
 		}
 
-		// Add headers
-		c.Writer.Header().Set("Subscription-Userinfo", header)
-		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
-		c.Writer.Header().Set("Profile-Title", subId)
-
-		if a.subEncrypt {
-			c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
+	} else {
+		host := strings.Split(c.Request.Host, ":")[0]
+		subs, header, err := a.subService.GetSubs(subId, host)
+		if err != nil || len(subs) == 0 {
+			c.String(400, "Error!")
 		} else {
-			c.String(200, result)
+			result := ""
+			for _, sub := range subs {
+				result += sub + "\n"
+			}
+
+			// Add headers
+			c.Writer.Header().Set("Subscription-Userinfo", header)
+			c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
+			c.Writer.Header().Set("Profile-Title", subId)
+
+			if a.subEncrypt {
+				c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
+			} else {
+				c.String(200, result)
+			}
 		}
 	}
 }
